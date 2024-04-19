77628b7ae32ce033cd2f219795894aeb369c769c
khunnnnnnn <nunnun.234@gmail.com>
2024-04-19 17:35:40 +0700

    new template on login page

--- public/src/pages/Login.jsx
+++ public/src/pages/Login.jsx
@@ -11,12 +11,13 @@ export default function Login() {
   const navigate = useNavigate();
   const [values, setValues] = useState({ username: "", password: "" });
   const toastOptions = {
-    position: "bottom-right",
-    autoClose: 8000,
+    position: 'top-center',
+    autoClose: 5000,
     pauseOnHover: true,
-    draggable: true,
-    theme: "dark",
+    draggable: false,
+    theme: "light",
   };
+
   useEffect(() => {
     if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
       navigate("/");
@@ -64,10 +65,9 @@ export default function Login() {
   return (
     <>
       <FormContainer>
-        <form action="" onSubmit={(event) => handleSubmit(event)}>
+        <form action="" onSubmit={(event) => handleSubmit(event)} >
           <div className="brand">
-            <img src={Logo} alt="logo" />
-            <h1>J H N P</h1>
+            <h1>เตเต้มาเรียนบ้าง!</h1>
           </div>
           <input
             type="text"
@@ -84,7 +84,7 @@ export default function Login() {
           />
           <button type="submit">Log In</button>
           <span>
-            Don't have an account ? <Link to="/register">Create One.</Link>
+            Don't have an account ? <Link to="/register">Register Now</Link>
           </span>
         </form>
       </FormContainer>
@@ -101,7 +101,7 @@ const FormContainer = styled.div`
   justify-content: center;
   gap: 1rem;
   align-items: center;
-  background-color: #131324;
+  background-color: #E3EEE2;
   .brand {
     display: flex;
     align-items: center;
@@ -113,6 +113,8 @@ const FormContainer = styled.div`
     h1 {
       color: white;
       text-transform: uppercase;
+      font-weight: 600;
+      color: #403129;
     }
   }
 
@@ -120,44 +122,61 @@ const FormContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 2rem;
-    background-color: #00000076;
-    border-radius: 2rem;
+    background-color: #9CC599;
+    border-radius: 0.5rem;
     padding: 5rem;
+    border: 3px solid #403129;
+    box-shadow: 0px 0px 30px 5px #403129;
   }
   input {
-    background-color: transparent;
-    padding: 1rem;
-    border: 0.1rem solid #4e0eff;
-    border-radius: 0.4rem;
-    color: white;
+    background-color: #bad6b8;
+    padding-left: 1rem;
+    padding-top: 0.75rem;
+    padding-bottom: 0.75rem;
+    border: 2px solid #403129;
+    border-radius: 0.2rem;
+    box-shadow: 1.5px 1.5px #403129;
+    color: #403129;
     width: 100%;
     font-size: 1rem;
+    font-weight: 600;
+    transition: 0.2s;
+    &:hover {
+      background-color: #cee2cd;
+      outline: none;
+    }
     &:focus {
-      border: 0.1rem solid #997af0;
+      background-color: #cee2cd;
       outline: none;
     }
   }
   button {
-    background-color: #4e0eff;
-    color: white;
-    padding: 1rem 2rem;
-    border: none;
+    background-color: #7c5f4f;
+    color: whitesmoke;
+    padding: 0.75rem 2rem;
+    border: 2px solid #403129;
+    box-shadow: 1.5px 1.5px #403129;
     font-weight: bold;
     cursor: pointer;
     border-radius: 0.4rem;
     font-size: 1rem;
     text-transform: uppercase;
+    transition: 0.2s;
     &:hover {
-      background-color: #4e0eff;
+      background-color: #5e483c;
     }
   }
   span {
-    color: white;
-    text-transform: uppercase;
+    color: #403129;
+    font-weight: 500;
     a {
-      color: #4e0eff;
-      text-decoration: none;
-      font-weight: bold;
+      color: #5e483c;
+      text-transform: uppercase;
+      font-weight: 700;
+      transition: 0.2s;
+      &:hover {
+        color: #9a7662;
+      }
     }
   }
 `;
